cdbcd4ea3a033f561d72f51b17a6f843aa173fb8
pridom1118 <pridom1118@gmail.com>
2023-12-19 17:41:52 +0900

    쉘 스크립트 수정

--- kernel_tuning.sh
+++ kernel_tuning.sh
@@ -5,6 +5,17 @@
 # some kernel and system settings should be tuned.
 # Please run this script on superuser to disable swap.
 
+RES = $(stat -fc %T /sys/fs/cgroup)
+
+if [ "$RES" != "tmpfs" ]; then
+    echo "cgroup v2 -- isolate is not supported in this environment!\n"
+    echo "add kernel parameters below:\n"
+    echo "systemd.unified_cgroup_hierarchy=false systemd.legacy_systemd_cgroup_controller=false\n"
+    echo "sudo vi /etc/default/grub and add above parameters to GRUB_CMDLINE_LINUX_DEFAULT, after quiet splash\n"
+    echo "then run sudo update-grub\n"
+    exit 1
+fi
+
 echo 0 > /proc/sys/kernel/randomize_va_space 
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 echo never > /sys/kernel/mm/transparent_hugepage/defrag
